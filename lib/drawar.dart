e105607b068c04ce4b8b5849759b828dea58f431
Waqad <waqadarshad2@gmail.com>
2022-04-13 14:53:12 +0500

    drawer Complete

--- lib/drawar.dart
+++ lib/drawar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -5,6 +6,57 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+              padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+                child:UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  accountName: Text("Shamsi"),
+                  accountEmail: Text("sheharyarshamsi512@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("assets/login.png"),
+                  ),
+
+                ),
+            ),
+            ListTile(
+              title:Text("Home",
+                textScaleFactor: 1.2,
+                style:TextStyle(color:Colors.white),
+              ), leading: Icon(CupertinoIcons.home,
+                  color:Colors.white,
+              ),
+
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title:Text("Profile",
+                  textScaleFactor:1.2,
+                style: TextStyle(
+                  color:Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.mail,
+              color:Colors.white),
+              title:Text("Mail",
+              style: TextStyle(
+                color: Colors.white
+              ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
